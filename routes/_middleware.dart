baa52ee1b3aabc57c83f5272f5dd7919fc39ed44
Dhemas Nurjaya <dhemasnurjaya@gmail.com>
2024-09-02 07:44:14 +0700

    add cors middleware

--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -2,12 +2,31 @@ import 'package:dart_frog/dart_frog.dart';
 import 'package:dart_frog_auth/dart_frog_auth.dart';
 import 'package:kuwot_api/core/auth/simple_auth.dart';
 
+import 'package:shelf_cors_headers/shelf_cors_headers.dart' as shelf;
+
 Handler middleware(Handler handler) {
-  // add request logger middleware
-  return handler.use(_authCheck).use(requestLogger());
+  // chain the middlewares
+  return handler
+      .use(
+        fromShelfMiddleware(
+          shelf.corsHeaders(
+            headers: {
+              shelf.ACCESS_CONTROL_ALLOW_ORIGIN:
+                  'https://kuwot-api.dhemasnurjaya.com',
+            },
+          ),
+        ),
+      )
+      .use(_authCheck)
+      .use(requestLogger());
 }
 
 Handler _authCheck(Handler handler) {
+  const appliedRoutes = [
+    '/quotes',
+    '/images',
+  ];
+
   return handler.use(
     bearerAuthentication<bool>(
       authenticator: (context, token) async {
@@ -20,6 +39,9 @@ Handler _authCheck(Handler handler) {
 
         final isTokenValid = authenticator.isTokenValid(decryptedToken);
         return isTokenValid ? true : null;
+      },
+      applies: (RequestContext context) async {
+        return appliedRoutes.any((e) => context.request.uri.path.startsWith(e));
       },
     ),
   );
